65b5ea4aad05b22105c3b09f1b99af2fe84608fc
Chad Phillips <chad@apartmentlines.com>
2023-08-28 13:49:55 -0600

    use flake8 instead of pyflakes

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -51,6 +51,7 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
         -- "stylua",
+        "flake8",
       })
     end,
   },
