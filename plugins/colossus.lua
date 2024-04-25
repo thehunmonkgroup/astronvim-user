963bfda073578e8a86c0e2e2ee929568485da5f2
Chad Phillips <chad@apartmentlines.com>
2024-04-25 11:43:09 -0400

    upgrade chatgpt.nvim config to gpt-4-turbo

--- plugins/colossus.lua
+++ plugins/colossus.lua
@@ -12,7 +12,7 @@ if os.getenv("OPENAI_API_KEY") then
           },
         },
         openai_params = {
-          model = "gpt-4-1106-preview",
+          model = "gpt-4-turbo",
           max_tokens = 1500,
         },
         openai_edit_params = {
