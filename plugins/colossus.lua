e2c192bf90a306a92c5230cc70772215c8093369
Chad Phillips <chad@apartmentlines.com>
2024-10-06 21:16:27 -0400

    switch to 4o-mini

--- plugins/colossus.lua
+++ plugins/colossus.lua
@@ -16,7 +16,7 @@ if os.getenv("OPENAI_API_KEY") then
           max_tokens = 1500,
         },
         openai_edit_params = {
-          model = "gpt-4-1106-preview",
+          model = "gpt-4o-mini",
         },
         use_openai_functions_for_edits = true,
       },
